0df50fd8c4fbcd69d6331bf9a3b709dab7b70182
Shane Osbourne <shane.osbourne8@gmail.com>
2017-05-18 20:50:36 +0100

    failing test for duplicate interfaces

---
+++ __tests__/dup.js
@@ -0,0 +1,36 @@
+const {json2ts} = require('../');
+const assert = require('assert');
+
+const json = `{
+  "summary": {
+    "loc": {
+        "lat": 10, 
+        "lng": 11
+    }
+  },
+  "loc": {
+    "lat": 10, 
+    "lng": 11
+  }
+}
+`;
+
+const expected = `
+interface IRootObject {
+  summary: ISummary;
+  loc: ILoc;
+}
+
+interface ISummary {
+  loc: ILoc;
+}
+
+interface ILoc {
+  lat: number;
+  lng: number;
+}
+`;
+
+it('works 2 levels of depth', function() {
+    expect(json2ts(json)).toEqual(expected.slice(1));
+});
