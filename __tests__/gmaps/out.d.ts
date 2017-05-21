9c9d3918fa5ad6ccc9e4cced19af4a1393d49707
Shane Osbourne <shane.osbourne8@gmail.com>
2017-05-21 20:35:19 +0100

    fix gmaps example

--- __tests__/gmaps/out.d.ts
+++ __tests__/gmaps/out.d.ts
@@ -1,51 +1,36 @@
 interface IRootObject {
-  results: IResultsItem[];
-  status: string;
+    results: IResultsItem[];
+    status: string;
 }
 
 interface IResultsItem {
-  address_components: IAddressComponentsItem[];
-  formatted_address: string;
-  geometry: IGeometry;
-  place_id: string;
-  types: string[];
+    address_components: IAddressComponentsItem[];
+    formatted_address: string;
+    geometry: IGeometry;
+    place_id: string;
+    types: string[];
 }
 
 interface IAddressComponentsItem {
-  long_name: string;
-  short_name: string;
-  types: string[];
+    long_name: string;
+    short_name: string;
+    types: string[];
 }
 
 interface IGeometry {
-  bounds: IBounds;
-  location: ILocation;
-  location_type: string;
-  viewport: IViewport;
+    bounds: IBounds;
+    location: INortheast;
+    location_type: string;
+    viewport: IBounds;
 }
 
 interface IBounds {
-  northeast: INortheast;
-  southwest: ISouthwest;
+    northeast: INortheast;
+    southwest: INortheast;
 }
 
 interface INortheast {
-  lat: number;
-  lng: number;
-}
-
-interface ISouthwest {
-  lat: number;
-  lng: number;
-}
-
-interface ILocation {
-  lat: number;
-  lng: number;
-}
-
-interface IViewport {
-  northeast: INortheast;
-  southwest: ISouthwest;
+    lat: number;
+    lng: number;
 }
 
